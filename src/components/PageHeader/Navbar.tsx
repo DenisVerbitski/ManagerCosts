7a3ea596254d1a58ca735c08bcc98713b17f3e26
iYaSergey <ya.sergey137@gmail.com>
2021-04-30 14:19:38 +0300

    Refactored navbar

--- src/components/PageHeader/Navbar.tsx
+++ src/components/PageHeader/Navbar.tsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { IFormData, ModalComponent, } from "../Modal/ModalComponent/ModalComponent";
+import {
+  IFormData,
+  ModalComponent,
+} from "../Modal/ModalComponent/ModalComponent";
 import styles from "../PageHeader/Navbar.less";
 import { PageHeader } from "antd";
 
-//className = {styles.selector} - импорт стилей
 interface NavbarProps {
-  onCreateElement: (values: IFormData) => void
+  onCreateElement: (values: IFormData) => void;
 }
+
 export const Navbar = (props: NavbarProps) => {
   return (
     <div>
       <PageHeader
         className={styles.sitepageheader}
         title="Менеджер Расходов"
-        extra={[<ModalComponent onCreateElement = {props.onCreateElement} />]}
+        extra={[<ModalComponent onCreateElement={props.onCreateElement} />]}
       />
     </div>
   );
