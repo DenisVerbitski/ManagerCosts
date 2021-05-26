88ec527bd94a21c943458171c0ddef0d73972d12
DenisVerbitski <denisverbitski20@gmail.com>
2021-05-26 15:08:37 +0300

    AddLocalStorage

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import spentsDataReducer from "./reducers/spentsData";
+import { loadState, saveState } from "./localstorage";
 
 export const store = configureStore({
   reducer: {
     spentsDataReducer,
   },
+  preloadedState: loadState(),
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+
+store.subscribe(() => saveState(store.getState()));
